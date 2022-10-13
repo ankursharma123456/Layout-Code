315a3876dafbcc06d9ca11f6504bc3c081c1c518
ankursharma123456 <101103443+ankursharma123456@users.noreply.github.com>
2022-10-13 11:02:25 +0530

    Add files via upload

---
+++ layout/layout.js
@@ -0,0 +1,19 @@
+import { LightningElement,api } from 'lwc';
+
+export default class Layout extends LightningElement {
+    
+    @api image = '';
+    @api name = '';
+    @api position = '';
+    @api technology = '';
+    @api image1 ='';
+ @api name1 = '';
+ @api position1 = '';
+ @api technology1 = '';
+ @api image2 = '';
+ @api name2 = '';
+ @api position2 = '';
+ @api technology2 = '';
+
+ 
+}
